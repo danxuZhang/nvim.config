39c19a02a9c870a25cd55aec3bed9b7c4ffd32bf
dxZhang <dxzhang49@outlook.com>
2024-10-17 09:06:00 +0800

    Avoid duplicated git diff mapping

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,7 +2,6 @@ return {
     "tpope/vim-fugitive",
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-        vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<CR>")
-        vim.keymap.set("n", "<leader>gb", ":Git blame<CR>")
+        vim.keymap.set("n", "<leader>df", ":Gvdiffsplit<CR>")
     end,
 }
